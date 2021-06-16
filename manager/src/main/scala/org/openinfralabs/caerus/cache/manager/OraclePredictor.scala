d0e2f0885196ba02db6a1a64936a601c637d0228
tedgoud-fw <theo.gkountouvas@futurewei.com>
2021-06-16 11:17:20 -0400

    Changed oracle predictor.

--- manager/src/main/scala/org/openinfralabs/caerus/cache/manager/OraclePredictor.scala
+++ manager/src/main/scala/org/openinfralabs/caerus/cache/manager/OraclePredictor.scala
@@ -1,11 +1,23 @@
 package org.openinfralabs.caerus.cache.manager
 
+import com.typesafe.scalalogging.LazyLogging
 import org.openinfralabs.caerus.cache.common.plans.CaerusPlan
 
-case class OraclePredictor(var futurePlans: Seq[CaerusPlan]) extends Predictor {
+case class OraclePredictor(var futurePlans: Seq[CaerusPlan]) extends Predictor with LazyLogging {
+
+
   override def getPredictions(caerusPlan: CaerusPlan): Seq[CaerusPlan] = {
-    assert(futurePlans.nonEmpty && caerusPlan == futurePlans.head)
-    futurePlans = futurePlans.tail
-    futurePlans
+    if (futurePlans.isEmpty) {
+      logger.warn("Oracle predictions are empty.")
+      futurePlans
+    } else {
+      if (!futurePlans.head.equals(caerusPlan)) {
+        logger.warn(
+          "Caerus plan does not match the oracle prediction. Plans:\n%s\n%s".format(caerusPlan, futurePlans.head)
+        )
+      }
+      futurePlans = futurePlans.tail
+      futurePlans
+    }
   }
 }
